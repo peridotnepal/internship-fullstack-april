21265ab2b031490526989a9caf0bf397b89b2cd0
rahulcoder2 <rahulchaudharycsit077@indrenicollege.edu.np>
2025-04-12 00:11:24 +0545

    update: api.ts fs file path and more task1-sampledate.json  in client

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -2,8 +2,7 @@
 import { promises as fs } from "fs";
 
 export default async function getFinancialData() {
-  const file = await fs.readFile(
-    "/home/litboy/coding/web development/nextjs/internship-fullstack-april/task1-sampledata.json",
+  const file = await fs.readFile(process.cwd()+ "/task1-sampledata.json",
     "utf8"
   );
   const data = JSON.parse(file);
